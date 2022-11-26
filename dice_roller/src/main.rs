80862f98d64de54af087e6f379ce7866638c1b4d
HuoKnight <noman.gronckle@gmail.com>
2022-11-26 00:18:27 -0800

    sleeping

--- dice_roller/src/main.rs
+++ dice_roller/src/main.rs
@@ -1,5 +1,11 @@
 use rand::Rng;
 use std::collections::HashMap;
+use colored::*;
+
+fn crash(code: i32) {
+    println!("{}", "Please make sure your arguments are correct.\n Syntax: {command} {args} {{# to roll}d{# sides}}}".red());
+    std::process::exit(code);
+}
 
 fn hashmap_init(upper: u32) -> HashMap<u32, u32> {
     let mut hashmap = HashMap::new();
@@ -13,7 +19,15 @@ fn hashmap_init(upper: u32) -> HashMap<u32, u32> {
     hashmap
 }
 
-fn roll(amount: &str, sides: &str) {
+fn roll(amount: &str, sides: &str, arguments: HashMap<&str, bool>) {
+    match amount.parse::<u32>() {
+        Ok(_v) => println!("OK"),
+        Err(_e) => crash(1),
+    }
+    match sides.parse::<u32>() {
+        Ok(_v) => println!("OK"),
+        Err(_e) => crash(1),
+    }
     let iterator = amount.parse::<u32>().unwrap();
     let upper = sides.parse::<u32>().unwrap();
     let mut pool = rand::thread_rng();
@@ -28,7 +42,11 @@ fn roll(amount: &str, sides: &str) {
         println!("{}: {}", die, num);
     }
     println!("# Max Rolls: {}", max_count);
-
+    if arguments["print_results"] == true {
+        for (key, val) in result_index {
+            println!("{} - {}", key, val);
+        }
+    }
     // println!("{:?}", result_index);
     // println!("{}, {}", amount, sides)
 }
@@ -37,13 +55,36 @@ fn roll(amount: &str, sides: &str) {
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     // println!("{:?}", args);
-    let die: Vec<&str> = args[1].split('d').collect();
+    let die: Vec<&str> = args.last().unwrap().split('d').collect();
 
-    // if args[2] == "-v" {
-    //     let verbose = true;
-    // }
 
+    let mut switches = HashMap::from([
+        ("print_results", false),
+        ("count_one", false),
+    ]);
+    for arg in &args {
+        if arg == "-r" {
+            switches.insert(
+                "print_results",
+                true,
+            );
+        }
+        else if arg == "-c" {
+            switches.insert(
+                "count_one",
+                true,
+            );
+        }
+    }
+    println!("{:?}", switches);
     // println!("{:?}", die);
-    println!("Rolling: {}", args[1]);
-    roll(die[0], die[1]);
+    println!("Rolling: {}", &args.last().unwrap());
+    let test_if_die = &die.len();
+    match test_if_die {
+        2 => if &die[1] == &"" {
+            crash(1);
+        },
+        _ => crash(1),
+    };
+    roll(die[0], die[1], switches);
 }
